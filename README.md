2e5fc4eb5b8991408d38a3c4903ede7094da3892
liguang <liguang@wicture.com>
2017-01-08 16:59:35 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # static-resource-hash-webpack-plugin
 基于WebPack HTML/CSS/JS/Image Hash功能。实现逻辑为 当所有文件生成后，通过查找文件引用来更新Hash后的文件名。不支持动态路径引用。
+
+## 使用说明
+
+== 在WebPack中使用时，要将此模块放置在最后，因为模块的执行是在最终生成的文件中进行的。 ==
+== 依赖crypto及glob ==
+
+第一步，引用模块：
+```javascript
+var StaticResHashPlugin = require('static-resource-hash-webpack-plugin')
+```
+
+第二步，加入到**webpack plugins**中：
+```javascript
+new StaticResHashPlugin({
+  log: true,
+  hashLength: 8,
+  destRoot: 'dist',
+  hashFiles: ['/img/**/*', '/components/**/*.html', '/fonts/**/*', '/*.js', '/404.html', '/css/*.css' ],
+  hostFiles: ['/**/*.js', '/**/*.html', '/css/*.css']
+})
+```
+- **log** 默认值为false，不输出Log信息
+- **hashLength** Hash值的长度，默认为8位，一般无需指定
+- **destRoot** 执行此Hash操作的目录，应该为输出目录
+- **hashFiles** 执行Hash操作的文件，即需要对哪些文件进行Hash重命名操作
+- **hostFiles** 在哪些文件里面查找文件引用，并执行替换
+
---
+++ index.js
@@ -0,0 +1,137 @@
+var fs = require('fs');
+var path = require('path');
+var glob = require('glob');
+var crypto = require('crypto');
+
+var hashFileObjects = [];
+var filePatterns = [
+  /(url\s*\( *)([^:\*\?<>'"\|\(\)@;]*\.[^:\*<>'"\|\(\)@;]+)( *['"]*\))/g,
+  /(')([^:\*\?<>'"\|\(\)@;]*\.[^:\*<>'"\|\(\)@;]+)(')/g,
+  /(")([^:\*\?<>'"\|\(\)@;]*\.[^:\*<>'"\|\(\)@;]+)(")/g,
+]
+
+/*
+  {
+  	log: false,
+    hashLength: 8,
+    destRoot: 'app',
+    hashFiles: ['/**//*.js', '/**//*.css'],
+    hostFiles: ['/**//*.js', '/**//*.html', '/**//*.css'],
+  }
+* */
+function StaticResHashPlugin(options) {
+  if (!options) options = {};
+  if (!options.hashFiles){
+    console.error('hashFiles is needed!');
+  }
+  if (!options.hostFiles){
+    console.error('hostFiles is needed!');
+  }
+  if (options.destRoot === undefined) {
+    console.error('destRoot is needed!');
+  }
+
+  this.options = options;
+  this.options.hashLength = options.hashLength || 8;
+}
+
+var globOpts;
+
+StaticResHashPlugin.prototype.apply = function(compiler){
+  this.compiler = compiler
+  compiler.plugin('done', function(stats){
+    globOpts = {
+      root: path.resolve(path.join(process.cwd(), this.options.destRoot)),
+      nodir: true,
+    };
+
+		globOpts.log && console.log('current context path : ' + process.cwd());
+
+    var hashFiles = this.options.hashFiles;
+    var hostFiles = this.options.hostFiles;
+    var hashLength = this.options.hashLength;
+    var tmpFiles, hashFilExist;
+    for(var i=0;i<hashFiles.length;i++){
+      tmpFiles = glob.sync(hashFiles[i], globOpts);
+      for(var j=0;j<tmpFiles.length;j++){
+        var obj = {
+          path: path.resolve(tmpFiles[j]),
+          hash: calcHashByFile(tmpFiles[j], hashLength)
+        };
+        obj.ext = path.extname(tmpFiles[j]);
+        obj.name = path.basename(tmpFiles[j], obj.ext);
+        obj.hashBaseName = obj.name + '.' + obj.hash + obj.ext;
+        obj.hashPath = path.join(path.dirname(obj.path), obj.hashBaseName);
+
+        hashFilExist = false;
+        for(var m=0;m<hashFileObjects.length;m++){
+          if(hashFileObjects[m].path === obj.path){
+            hashFilExist = true;
+            break;
+          }
+        }
+        !hashFilExist && hashFileObjects.push(obj);
+      }
+    }
+
+    for(i=0;i<hostFiles.length;i++){
+      tmpFiles = glob.sync(hostFiles[i], globOpts);
+      for(j=0;j<tmpFiles.length;j++){
+        updateHashRef(tmpFiles[j]);
+      }
+    }
+
+    //rename hashFiles
+    for(i=0;i<hashFileObjects.length;i++){
+			globOpts.log && console.log('file [' + hashFileObjects[i].path + '] execute rename --> [' + hashFileObjects[i].hashPath + ']');
+      fs.renameSync(hashFileObjects[i].path, hashFileObjects[i].hashPath);
+    }
+
+  }.bind(this))
+}
+
+
+function calcHashByFile(fp, length){
+  var cont = fs.readFileSync(fp, 'utf8');
+  var shaHasher = crypto.createHash('sha1');
+  shaHasher.update(cont);
+  return shaHasher.digest('hex').slice(0, length);
+}
+
+function updateHashRef(hostFilePath){
+  var cont = fs.readFileSync(hostFilePath, 'utf8');
+  for(var i=0;i<filePatterns.length;i++){
+    cont = cont.replace(filePatterns[i], function(cont, sm01, link, sm03){
+      var ext = path.extname(hostFilePath).toUpperCase();
+      var dir = path.dirname(hostFilePath);
+      var linkAbsPath;
+      var originalLink = link;
+      var queryStartIndex = link.indexOf('?');
+      var queryString = '';
+      if(queryStartIndex > -1){
+        link = link.substr(0, queryStartIndex);
+        queryString = originalLink.substring(queryStartIndex, originalLink.length);
+      }
+      //path in css. path begin with dot or virgule is relative.
+      if('.CSS' === ext && (link.startsWith('.') || (!link.startsWith('/') && !link.startsWith('\\')))){
+        linkAbsPath = path.join(dir, link);
+      }else{
+        linkAbsPath = path.join(globOpts.root, link);
+      }
+      if(linkAbsPath && fs.existsSync(linkAbsPath)){
+        //find hashvalue from hashFiles
+        for(var j=0;j<hashFileObjects.length;j++){
+          if(hashFileObjects[j].path === linkAbsPath){
+						globOpts.log && console.log('file [' + hostFilePath + '] execute replace [' + link + '] --> [' + hashFileObjects[j].hashBaseName + ']')
+            return sm01 + path.join(path.dirname(link), hashFileObjects[j].hashBaseName).replace(/\\/g, '/') + queryString + sm03;
+          }
+        }
+      }
+      return cont;
+    });
+  }
+  fs.writeFileSync(hostFilePath, cont);
+}
+
+module.exports = StaticResHashPlugin
+
